1b713585cbfb37f333dc9efe7edeb5a36102c81c
parris3142 <parris3142@hotmail.com>
2023-01-22 09:19:22 +0800

    Simple change to test commit from remix ide

--- contracts/stable-coins/ERC20StableCoin.sol
+++ contracts/stable-coins/ERC20StableCoin.sol
@@ -1,6 +1,8 @@
 // contracts/OceanToken.sol
 // SPDX-License-Identifier: MIT
 
+// Base contract for ERC20 Based Stable Coin
+
 pragma solidity ^0.8.17;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
