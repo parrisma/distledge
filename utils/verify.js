2627ca6e93fb258b1614b757d64a388865cfe5ce
szh <szh>
2023-02-17 20:52:06 +0800

    [JSU] Formatted by prettier vs plugin.

--- utils/verify.js
+++ utils/verify.js
@@ -1,18 +1,16 @@
-const { run } = require('hardhat')
+const { run } = require("hardhat");
 
 //Common utility function to veify deployed contract by submiting the contract bytecode to etherscans.
 //The structure of smart contract is visible after verification is complete.
 const verify = async (contractAddress, args) => {
-    try{
-        await run("verify:verify",{
-            address:contractAddress,
-            constructorArguments: args
-        })
-    }catch(ex){
-        if(ex.name !== "NomicLabsHardhatPluginError")            
-            console.error(ex);        
-    }
-}
+  try {
+    await run("verify:verify", {
+      address: contractAddress,
+      constructorArguments: args,
+    });
+  } catch (ex) {
+    if (ex.name !== "NomicLabsHardhatPluginError") console.error(ex);
+  }
+};
 
-module.exports={ verify }
-
+module.exports = { verify };
