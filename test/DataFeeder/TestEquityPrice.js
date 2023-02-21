2679c869cba6e37045b80a9cbf40fa0516bd8540
szh <szh>
2023-02-21 21:41:14 +0800

    [JSU] Use waffle to mock chainlink contract instread of mock contract.

--- test/DataFeeder/TestEquityPrice.js
+++ test/DataFeeder/TestEquityPrice.js
@@ -1,24 +1,39 @@
 const { expect } = require("chai");
-const { ethers } = require('hardhat');
+const { Console } = require("console");
+const { ethers, waffle } = require("hardhat");
+const { provider, deployMockContract } = waffle;
+const { BigNumber } = ethers;
 
-describe("EquityPrice", function(){    
+describe("EquityPrice", function () {
+  it("Equity Price is equal to expected value", async function () {
+    //Create mock object for data from chain link which provides off chain data.
 
-    it("Equity Price is equal to expected value",async function(){        
-        
-        //Create mock object for data from chain link which provides off chain data.
-        const mock = await ethers.getContractFactory("MockV3Aggregator");
+    const [deployerOfContract] = await provider.getWallets();
+    const contractMeta = require("../../artifacts/@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol/AggregatorV3Interface.json");
 
-        //Define the mocked price.
-        const expected=50;
-        const mockDeployed = await mock.deploy(8,expected);
+    const mockContract = await deployMockContract(
+      deployerOfContract,
+      contractMeta.abi
+    );
 
-        //Create and deploy contract of equity price
-        const contract=await ethers.getContractFactory("EquityPrice");
-        const deployed=await contract.deploy(mockDeployed.address);        
+    //Define the mocked price.
+    const expected = 50;
 
-        //Get latest equity price
-        const actual = await deployed.getPrice();
-        
-        expect(Number(actual)).to.equal(expected);
-    })
-})
+    await mockContract.mock.latestRoundData.returns({
+      0: roundId,
+      expected: answer,
+      0: startedAt,
+      0: updatedAt,
+      0: answeredInRound,
+    });
+
+    //Create and deploy contract of equity price
+    const contract = await ethers.getContractFactory("EquityPrice");
+    const deployed = await contract.deploy(mockContract.address);
+
+    //Get latest equity price
+    const actual = await deployed.getPrice();
+
+    expect(Number(actual)).to.equal(expected);
+  });
+});
