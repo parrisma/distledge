895e5e1e02534280d5bae93c229345bf4ef0db79
szh <szh>
2023-02-22 20:06:26 +0800

    [JSU] Fix unit test issue.

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,28 +1,29 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("@nomiclabs/hardhat-web3");
-require("dotenv").config()
+require("@nomiclabs/hardhat-waffle");
+require("dotenv").config();
 
 //Assign environment variables for local use.
-const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL
-const PRIVATE_KEY = process.env.PRIVATE_KEY
-const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
+const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.17",
   defaultNetwork: "hardhat",
   networks: {
-      hardhat: {
-          blockGasLimit: 60000000 // Network block gasLimit
-      },
-      goerli: {
-        url: GOERLI_RPC_URL,  //test network RPC URL is defined in .env file.
-        accounts: [PRIVATE_KEY], //Personal account private key must be defined in .env file.
-        chainId: 5,
-        blockConfirmations: 6
-      }
+    hardhat: {
+      blockGasLimit: 60000000, // Network block gasLimit
+    },
+    goerli: {
+      url: GOERLI_RPC_URL, //test network RPC URL is defined in .env file.
+      accounts: [PRIVATE_KEY], //Personal account private key must be defined in .env file.
+      chainId: 5,
+      blockConfirmations: 6,
+    },
   },
-  etherscan:{
-    apiKey: {goerli:ETHERSCAN_API_KEY} //Etherscan API Key is generated from https://docs.etherscan.io/getting-started/viewing-api-usage-statistics
-  }
+  etherscan: {
+    apiKey: { goerli: ETHERSCAN_API_KEY }, //Etherscan API Key is generated from https://docs.etherscan.io/getting-started/viewing-api-usage-statistics
+  },
 };
--- test/DataFeeder/TestEquityPrice.js
+++ test/DataFeeder/TestEquityPrice.js
@@ -1,8 +1,6 @@
 const { expect } = require("chai");
-const { Console } = require("console");
 const { ethers, waffle } = require("hardhat");
 const { provider, deployMockContract } = waffle;
-const { BigNumber } = ethers;
 
 describe("EquityPrice", function () {
   it("Equity Price is equal to expected value", async function () {
@@ -19,13 +17,7 @@ describe("EquityPrice", function () {
     //Define the mocked price.
     const expected = 50;
 
-    await mockContract.mock.latestRoundData.returns({
-      0: roundId,
-      expected: answer,
-      0: startedAt,
-      0: updatedAt,
-      0: answeredInRound,
-    });
+    await mockContract.mock.latestRoundData.returns(0, expected, 0, 0, 0);
 
     //Create and deploy contract of equity price
     const contract = await ethers.getContractFactory("EquityPrice");
