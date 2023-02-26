618f6b9e830248d21240308feb99cc64a156c44a
parris3142 <parris3142@hotmail.com>
2023-02-26 16:47:29 +0530

    added time to live on Deal & dropped idea for Exchange contract, as not helpful for immediate demo

---
+++ contracts/Deals/Deal.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.17;
+
+import "hardhat/console.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+/*
+ ** All Deals must implment this abstract interface so they can be managed by the Exchange contract
+ */
+abstract contract Deal is Ownable {
+    /**
+     ** @notice Execute the deal (one shot)
+     */
+    function execute() public virtual returns (bool);
+
+    /**
+     ** @notice A ticker (symbol) for the deal
+     ** @return A globally unique string that for the type of deal.
+     */
+    function ticker() public virtual returns (string memory);
+
+    /**
+     ** @notice The number of seconds the deal has remaining before it cannot be executed
+     ** @return Number of seconds before deal cannot be executed.
+     */
+    function timeToLive() public view virtual returns (uint256);
+
+    /**
+     ** @notice The seller offering the deal.
+     ** @return address of seller offering the deal
+     */
+    function seller() public view virtual returns (address);
+}
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,3 +1,4 @@
+require("@nomicfoundation/hardhat-network-helpers");
 require("@nomicfoundation/hardhat-toolbox");
 require("@nomiclabs/hardhat-web3");
 require("dotenv").config();
