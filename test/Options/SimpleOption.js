ab7aa58c1105463e6f47a34bfdc20ad437f7664f
thatjamie <thatjamie.xie@gmail.com>
2023-03-10 23:31:17 +0800

    Validate the function valuation()

--- test/Options/SimpleOption.js
+++ test/Options/SimpleOption.js
@@ -18,7 +18,6 @@ const { ethers } = require("hardhat");
  * TODO: Complete test's use Integration.js for inspiration
  */
 
-const underlyingPrice = 1200;
 const genericDecimals = 2;
 const forexRate = 0.1;
 
@@ -37,6 +36,10 @@ describe("Simple Option Test Suite", function () {
   let equityPrice;
   let fxPrice;
   let simpleOption;
+
+  const premium = 10;
+  const notional = 100;
+  const strike = 1000;
 
   beforeEach(async function () {
     // Get accounts
@@ -62,26 +65,28 @@ describe("Simple Option Test Suite", function () {
     ).deploy();
 
     mockAggregator = await ethers.getContractFactory("MockV3Aggregator");
-    const mockUnderlyingPriceSource = await mockAggregator
-      .connect(data_vendor)
-      .deploy(genericDecimals, underlyingPrice);
     const mockForexSource = await mockAggregator
       .connect(data_vendor)
       .deploy(genericDecimals, forexRate * 10 ** genericDecimals);
+    const FXPrice = await ethers.getContractFactory("FXPrice");
+    fxPrice = await FXPrice.deploy("USD/CNY", mockForexSource.address); // fxReferenceLevel_ instance
+  });
+
+  it("Valuate contract as possible return when spot is greater than strike", async function () {
+    // Value of the contract is notional * (underlying price - strike price)
+    const underlyingPrice = 1200;
+    const mockUnderlyingPriceSource = await mockAggregator
+      .connect(data_vendor)
+      .deploy(genericDecimals, underlyingPrice);
     // await mockUnderlyingPriceSource.deployed();
     const EquityPrice = await ethers.getContractFactory("EquityPrice");
     equityPrice = await EquityPrice.deploy(
       "Underlying",
       mockUnderlyingPriceSource.address
     ); // referenceLevel_ instance
-    const FXPrice = await ethers.getContractFactory("FXPrice");
-    fxPrice = await FXPrice.deploy("USD/CNY", mockForexSource.address); // fxReferenceLevel_ instance
 
     const uniqueId = crypto.randomUUID();
     const SimpleOption = await ethers.getContractFactory("SimpleOption");
-    const premium = 1;
-    const notional = 100;
-    const strike = 1000;
     simpleOption = await SimpleOption.connect(option_seller).deploy(
       uniqueId,
       "SimpleOption",
@@ -96,9 +101,72 @@ describe("Simple Option Test Suite", function () {
       fxPrice.address
     );
     // await simpleOption.deployed();
+    expect(await simpleOption.connect(option_buyer).valuation()).to.equal(
+      20000
+    );
   });
 
-  it("Basic construction", async function () {
-    expect(true).to.equal(true); // TODO: Write tests.
+  it("Valuate contract as zero when spot is equal to strike", async function () {
+    // Value of the contract is notional * (underlying price - strike price)
+    const underlyingPrice = 1000;
+    const mockUnderlyingPriceSource = await mockAggregator
+      .connect(data_vendor)
+      .deploy(genericDecimals, underlyingPrice);
+    // await mockUnderlyingPriceSource.deployed();
+    const EquityPrice = await ethers.getContractFactory("EquityPrice");
+    equityPrice = await EquityPrice.deploy(
+      "Underlying",
+      mockUnderlyingPriceSource.address
+    ); // referenceLevel_ instance
+
+    const uniqueId = crypto.randomUUID();
+    const SimpleOption = await ethers.getContractFactory("SimpleOption");
+    simpleOption = await SimpleOption.connect(option_seller).deploy(
+      uniqueId,
+      "SimpleOption",
+      "SimpleOption",
+      option_buyer.address,
+      premium,
+      premiumToken.address,
+      settlementToken.address,
+      notional,
+      strike,
+      equityPrice.address,
+      fxPrice.address
+    );
+    // await simpleOption.deployed();
+    expect(await simpleOption.connect(option_buyer).valuation()).to.equal(0);
+  });
+
+  it("Valuate contract as zero when spot is lower than strike", async function () {
+    // Value of the contract is notional * (underlying price - strike price)
+    const underlyingPrice = 800;
+    const mockUnderlyingPriceSource = await mockAggregator
+      .connect(data_vendor)
+      .deploy(genericDecimals, underlyingPrice);
+    // await mockUnderlyingPriceSource.deployed();
+    const EquityPrice = await ethers.getContractFactory("EquityPrice");
+    equityPrice = await EquityPrice.deploy(
+      "Underlying",
+      mockUnderlyingPriceSource.address
+    ); // referenceLevel_ instance
+
+    const uniqueId = crypto.randomUUID();
+    const SimpleOption = await ethers.getContractFactory("SimpleOption");
+    simpleOption = await SimpleOption.connect(option_seller).deploy(
+      uniqueId,
+      "SimpleOption",
+      "SimpleOption",
+      option_buyer.address,
+      premium,
+      premiumToken.address,
+      settlementToken.address,
+      notional,
+      strike,
+      equityPrice.address,
+      fxPrice.address
+    );
+    // await simpleOption.deployed();
+    expect(await simpleOption.connect(option_buyer).valuation()).to.equal(0);
   });
 });
