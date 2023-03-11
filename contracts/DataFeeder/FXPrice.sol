585986e74f9ffeeb5749adfe4a436ac67f723181
parris3142 <parris3142@hotmail.com>
2023-03-11 21:20:07 +0800

    Updated equity price to use SecureLevel and amended tests

--- contracts/DataFeeder/FXPrice.sol
+++ contracts/DataFeeder/FXPrice.sol
@@ -4,25 +4,12 @@ pragma solidity ^0.8.17;
 import "./ReferenceLevel.sol";
 
 contract FXPrice is RefernceLevel {
-    constructor(string memory ticker_, address priceFeedAddress_)
-        RefernceLevel(ticker_, priceFeedAddress_)
-    {}
-
-    /**
-     ** @notice Get unique ticker name of the refernce level source
-     ** @return The unique ticker name
-     */
-    function getTicker() public view override returns (string memory) {
-        return _ticker;
-    }
-
-    /**
-     ** @notice Get lastest price from chainlink-data-feed which provides a secure, reliable, and decentralized source of off-chain data
-     ** @return The latest price as uint256 as equity prices are always >= 0
-     */
-    function getPrice() public view override returns (uint256) {
-        (, int256 price, , , ) = _priceFeed.latestRoundData();
-        require(price >= 0, "FXPrice: bad data feed, prices must be greater than zero");
-        return uint256(price);
+    constructor(
+        string memory ticker_,
+        string memory description_,
+        address priceFeedAddress_,
+        uint256 decimals_
+    ) RefernceLevel(ticker_, description_, priceFeedAddress_, decimals_) {
+        setGreaterThanZero();
     }
 }
--- test/DataFeeder/EquityPrice.js
+++ test/DataFeeder/EquityPrice.js
@@ -4,47 +4,78 @@ const {
 } = require("@nomicfoundation/hardhat-network-helpers");
 const { anyValue } = require("@nomicfoundation/hardhat-chai-matchers/withArgs");
 const { expect } = require("chai");
-const { ethers } = require("hardhat");
-const crypto = require("crypto");
+const crypto = require('crypto');
 
-describe("EquityPrice", function () {
-  let equityPrice;
-  let mockPriceSource;
-  let deployer;
-  const ticker = "TCKR";
-  beforeEach(async () => {
-    deployer = (await getNamedAccounts()).deployer;
-    // Deploy
-    await deployments.fixture(["EquityPrice"]);
-    equityPrice = await ethers.getContract("EquityPrice", deployer);
-    mockPriceSource = await ethers.getContract("MockV3Aggregator", deployer);
+const symbol = "SECS";
+const description = "Secure Source Description";
+const decimals = 2;
+
+describe("Equity Price Test Suite", function () {
+  async function deployAccountAndToken() {
+    // Get named accounts & deploy SecureLevel contract
+    const [owner, secure_source, other_source] = await ethers.getSigners();
+
+    // Deploy Library
+    const VerifySignerLib = await ethers.getContractFactory("VerifySigner");
+    const verifySignerLib = await VerifySignerLib.deploy();
+    await verifySignerLib.deployed();
+
+    const EquityPrice = await ethers.getContractFactory("EquityPrice", {
+      libraries: {
+        VerifySigner: verifySignerLib.address,
+      },
+    });
+    const equityPrice = await EquityPrice.connect(owner).deploy(symbol, description, secure_source.address, decimals);
+
+    return { equityPrice, owner, secure_source, other_source }
+  }
+
+  async function signedValue(signer, value) {
+    // Create a signed message
+    const nonce = Math.floor(Date.now());
+    const secretMessage = ethers.utils.solidityPack(["uint256", "uint256"], [value, nonce]);
+    const secretMessageHash = ethers.utils.keccak256(secretMessage);
+    const sig = await signer.signMessage(ethers.utils.arrayify(secretMessageHash)); // Now signed as secure source
+    return { value, nonce, sig }
+  }
+
+  it("Equity Price Construct and Set", async function () {
+
+    var { equityPrice, owner, secure_source, other_source } = await loadFixture(deployAccountAndToken);
+
+    var expectedPx = 50;
+    var { value, nonce, sig } = await signedValue(secure_source, expectedPx);
+    await expect(equityPrice.connect(secure_source).setVerifiedValue(value, nonce, ethers.utils.arrayify(sig)))
+      .to.emit(equityPrice, 'LevelUpdated')
+      .withArgs(value);
+
+    var expectedPx = expectedPx + 1;
+    var { value, nonce, sig } = await signedValue(secure_source, expectedPx);
+    await expect(equityPrice.connect(secure_source).setVerifiedValue(value, nonce, ethers.utils.arrayify(sig)))
+      .to.emit(equityPrice, 'LevelUpdated')
+      .withArgs(value);
   });
 
-  it("Equity Price is equal to initial price value and test updated price", async function () {
-    //Define the mocked price.
-    const expectedPx = 50;
-    expect(await equityPrice.getPrice()).to.equal(expectedPx);
-    expect(await equityPrice.getTicker()).to.equal(ticker);
+  it("Equity Price OK when set to zero", async function () {
 
-    // Update source and verify change
-    const revised = expectedPx + 1;
-    await mockPriceSource.updateAnswer(revised);
-    expect(await equityPrice.getPrice()).to.equal(revised);
+    var { equityPrice, owner, secure_source, other_source } = await loadFixture(deployAccountAndToken);
+
+    var expectedPx = 0;
+    var { value, nonce, sig } = await signedValue(secure_source, expectedPx);
+    await expect(equityPrice.connect(secure_source).setVerifiedValue(value, nonce, ethers.utils.arrayify(sig)))
+      .to.emit(equityPrice, 'LevelUpdated')
+      .withArgs(value);
   });
 
   it("Equity Price will be rejected when price is negative figure", async function () {
     //Define the bad price.
-    const expectedPx = -1;
-    await mockPriceSource.updateAnswer(expectedPx);
-    await expect(equityPrice.getPrice()).to.be.revertedWith(
-      "EquityPrice: bad data feed, the price must be greater than or equals to zero"
+    var { equityPrice, owner, secure_source, other_source } = await loadFixture(deployAccountAndToken);
+
+    var expectedPx = -1;
+    var { value, nonce, sig } = await signedValue(secure_source, expectedPx);
+    await expect(equityPrice.connect(secure_source).setVerifiedValue(value, nonce, ethers.utils.arrayify(sig))).to.be.revertedWith(
+      "SecureLevel: Constraint set for value update greater than or equal to zero"
     );
   });
 
-  it("Equity Price will be accepted when price equals to zero", async function () {
-    // boundary value price
-    const expectedPx = 0;
-    await mockPriceSource.updateAnswer(expectedPx);
-    expect(await equityPrice.getPrice()).to.equal(expectedPx);
-  });
 });
