a1c7b67e5831baa240aab77e0f9110d75848bdab
thatjamie <thatjamie.xie@gmail.com>
2023-03-12 16:25:33 +0800

    Rectify the Unit test for SimpleOption

--- test/Options/SimpleOption.js
+++ test/Options/SimpleOption.js
@@ -23,7 +23,7 @@ const forexRate = 0.1;
 
 describe("Simple Option Test Suite", function () {
   // Define accounts
-  //   let escrow_manager;
+  let escrow_manager;
   let stable_coin_issuer;
   let data_vendor;
   let option_seller;
@@ -40,11 +40,12 @@ describe("Simple Option Test Suite", function () {
   const premium = 10;
   const notional = 100;
   const strike = 1000;
+  const defaultTokenBalance = 10000000000000;
 
   beforeEach(async function () {
     // Get accounts
     [
-      //   escrow_manager,
+      escrow_manager,
       stable_coin_issuer,
       data_vendor,
       option_seller,
@@ -64,6 +65,60 @@ describe("Simple Option Test Suite", function () {
       stable_coin_issuer
     ).deploy();
 
+    const EscrowCurrenyAccount = await ethers.getContractFactory(
+      "EscrowCurrenyAccount"
+    );
+
+    const escrowPremiumTokenAccount = await EscrowCurrenyAccount.connect(
+      escrow_manager
+    ).deploy(premiumToken.address, 1);
+    await premiumToken.transferOwnership(
+      await escrowPremiumTokenAccount.connect(escrow_manager).contractAddress()
+    );
+    escrowPremiumTokenAccount.connect(escrow_manager).unPause();
+    const transId1 = crypto.randomUUID();
+    await escrowPremiumTokenAccount
+      .connect(escrow_manager)
+      .processDepositTransaction(
+        option_seller.address,
+        defaultTokenBalance,
+        transId1
+      );
+    const transId2 = crypto.randomUUID();
+    await escrowPremiumTokenAccount
+      .connect(escrow_manager)
+      .processDepositTransaction(
+        option_buyer.address,
+        defaultTokenBalance,
+        transId2
+      );
+
+    const escrowSettlementTokenAccount = await EscrowCurrenyAccount.connect(
+      escrow_manager
+    ).deploy(settlementToken.address, 1);
+    await settlementToken.transferOwnership(
+      await escrowSettlementTokenAccount
+        .connect(escrow_manager)
+        .contractAddress()
+    );
+    escrowSettlementTokenAccount.connect(escrow_manager).unPause();
+    const transId3 = crypto.randomUUID();
+    await escrowSettlementTokenAccount
+      .connect(escrow_manager)
+      .processDepositTransaction(
+        option_seller.address,
+        defaultTokenBalance,
+        transId3
+      );
+    const transId4 = crypto.randomUUID();
+    await escrowSettlementTokenAccount
+      .connect(escrow_manager)
+      .processDepositTransaction(
+        option_buyer.address,
+        defaultTokenBalance,
+        transId4
+      );
+
     mockAggregator = await ethers.getContractFactory("MockV3Aggregator");
     const mockForexSource = await mockAggregator
       .connect(data_vendor)
@@ -72,13 +127,12 @@ describe("Simple Option Test Suite", function () {
     fxPrice = await FXPrice.deploy("USD/CNY", mockForexSource.address); // fxReferenceLevel_ instance
   });
 
-  it("Valuate contract as possible return when spot is greater than strike", async function () {
+  it("Valuate contract as positive return when spot is greater than strike", async function () {
     // Value of the contract is notional * (underlying price - strike price)
     const underlyingPrice = 1200;
     const mockUnderlyingPriceSource = await mockAggregator
       .connect(data_vendor)
       .deploy(genericDecimals, underlyingPrice);
-    // await mockUnderlyingPriceSource.deployed();
     const EquityPrice = await ethers.getContractFactory("EquityPrice");
     equityPrice = await EquityPrice.deploy(
       "Underlying",
@@ -100,7 +154,18 @@ describe("Simple Option Test Suite", function () {
       equityPrice.address,
       fxPrice.address
     );
-    // await simpleOption.deployed();
+    const [tokenSymbol, premiumAmount] = await simpleOption.premium();
+    await premiumToken
+      .connect(option_buyer)
+      .approve(await simpleOption.contractAddress(), premiumAmount);
+
+    await premiumToken.allowance(
+      option_buyer.address,
+      simpleOption.contractAddress()
+    );
+    // Buyer approve premium transfer, so we can do the deal.
+    simpleOption.connect(option_buyer).acceptTerms();
+
     expect(await simpleOption.connect(option_buyer).valuation()).to.equal(
       20000
     );
@@ -112,7 +177,6 @@ describe("Simple Option Test Suite", function () {
     const mockUnderlyingPriceSource = await mockAggregator
       .connect(data_vendor)
       .deploy(genericDecimals, underlyingPrice);
-    // await mockUnderlyingPriceSource.deployed();
     const EquityPrice = await ethers.getContractFactory("EquityPrice");
     equityPrice = await EquityPrice.deploy(
       "Underlying",
@@ -134,7 +198,18 @@ describe("Simple Option Test Suite", function () {
       equityPrice.address,
       fxPrice.address
     );
-    // await simpleOption.deployed();
+    const [tokenSymbol, premiumAmount] = await simpleOption.premium();
+    await premiumToken
+      .connect(option_buyer)
+      .approve(await simpleOption.contractAddress(), premiumAmount);
+
+    await premiumToken.allowance(
+      option_buyer.address,
+      simpleOption.contractAddress()
+    );
+    // Buyer approve premium transfer, so we can do the deal.
+    simpleOption.connect(option_buyer).acceptTerms();
+
     expect(await simpleOption.connect(option_buyer).valuation()).to.equal(0);
   });
 
@@ -144,7 +219,6 @@ describe("Simple Option Test Suite", function () {
     const mockUnderlyingPriceSource = await mockAggregator
       .connect(data_vendor)
       .deploy(genericDecimals, underlyingPrice);
-    // await mockUnderlyingPriceSource.deployed();
     const EquityPrice = await ethers.getContractFactory("EquityPrice");
     equityPrice = await EquityPrice.deploy(
       "Underlying",
@@ -166,7 +240,18 @@ describe("Simple Option Test Suite", function () {
       equityPrice.address,
       fxPrice.address
     );
-    // await simpleOption.deployed();
+    const [tokenSymbol, premiumAmount] = await simpleOption.premium();
+    await premiumToken
+      .connect(option_buyer)
+      .approve(await simpleOption.contractAddress(), premiumAmount);
+
+    await premiumToken.allowance(
+      option_buyer.address,
+      simpleOption.contractAddress()
+    );
+    // Buyer approve premium transfer, so we can do the deal.
+    simpleOption.connect(option_buyer).acceptTerms();
+
     expect(await simpleOption.connect(option_buyer).valuation()).to.equal(0);
   });
 });
