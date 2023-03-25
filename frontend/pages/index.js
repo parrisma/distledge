cbb5db535b1530be5039438eda4cdf857015b161
parris3142 <parris3142@hotmail.com>
2023-03-26 04:45:08 +0530

    Added new react token model and updated UI to add all stable conis

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -3,6 +3,7 @@ import Image from "next/image";
 import styles from "@/styles/Home.module.css";
 import Header from "../components/Header";
 import Price from "../components/Price";
+import StableToken from "../components/StableToken";
 import { addressConfig } from "../constants";
 
 export default function Home() {
@@ -16,18 +17,44 @@ export default function Home() {
           <link rel="icon" href="/favicon.ico" />
         </Head>
         <Header />
-        <Price contract={{
-          address: addressConfig["teslaEquityPriceContract"]
-            ? addressConfig["teslaEquityPriceContract"]
-            : null,
-          type: "equity"
-        }} />
-        <Price contract={{
-          address: addressConfig["UsdEurFXRateContract"]
-            ? addressConfig["UsdEurFXRateContract"]
-            : null,
-          type: "fx"
-        }} />
+        <div className="div-table">
+          <div className="div-table-row">
+            <div className="div-table-col-fix-wide">
+              <Price contract={{
+                address: addressConfig["teslaEquityPriceContract"]
+                  ? addressConfig["teslaEquityPriceContract"]
+                  : null,
+                type: "equity"
+              }} />
+            </div>
+            <div className="div-table-col-fix-wide">
+              <StableToken contract={{
+                address: addressConfig["usdStableCoin"]
+                  ? addressConfig["usdStableCoin"]
+                  : null,
+                type: "usdStableCoin"
+              }} />
+            </div>
+          </div>
+          <div className="div-table-row">
+            <div className="div-table-col-fix-wide">
+              <Price contract={{
+                address: addressConfig["UsdEurFXRateContract"]
+                  ? addressConfig["UsdEurFXRateContract"]
+                  : null,
+                type: "fx"
+              }} />
+            </div>
+            <div className="div-table-col-fix-wide">
+              <StableToken contract={{
+                address: addressConfig["eurStableCoin"]
+                  ? addressConfig["eurStableCoin"]
+                  : null,
+                type: "eurStableCoin"
+              }} />
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
