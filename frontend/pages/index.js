246006ea3f9a255ffeea44fe809826a30f6e5ee9
Tyrus <jactiction@gmail.com>
2023-03-26 12:58:42 +0800

    pass values through components

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -3,11 +3,8 @@ import Image from "next/image";
 import styles from "@/styles/Home.module.css";
 import Header from "../components/Header";
 import Price from "../components/Price";
-<<<<<<< HEAD
 import EscrowManager from "../components/EscrowManager";
-=======
 import StableToken from "../components/StableToken";
->>>>>>> main
 import { addressConfig } from "../constants";
 import OutputWindow from "../components/OutputWindow";
 import { useState } from "react";
@@ -37,28 +34,55 @@ export default function Home() {
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <link rel="icon" href="/favicon.ico" />
         </Head>
-<<<<<<< HEAD
         <Header onHeaderTabChange={handleTabChange} />
         {activeTab === "Price" ? (
-          <div>
-            <Price
-              contract={{
-                address: addressConfig["teslaEquityPriceContract"]
-                  ? addressConfig["teslaEquityPriceContract"]
-                  : null,
-                type: "equity",
-              }}
-              onAddInfo={handleLogChange}
-            />
-            <Price
-              contract={{
-                address: addressConfig["UsdEurFXRateContract"]
-                  ? addressConfig["UsdEurFXRateContract"]
-                  : null,
-                type: "fx",
-              }}
-              onAddInfo={handleLogChange}
-            />
+          <div className="div-table">
+            <div className="div-table-row">
+              <div className="div-table-col-fix-wide">
+                <Price
+                  contract={{
+                    address: addressConfig["teslaEquityPriceContract"]
+                      ? addressConfig["teslaEquityPriceContract"]
+                      : null,
+                    type: "equity",
+                  }}
+                  onAddInfo={handleLogChange}
+                />
+              </div>
+              <div className="div-table-col-fix-wide">
+                <StableToken
+                  contract={{
+                    address: addressConfig["usdStableCoin"]
+                      ? addressConfig["usdStableCoin"]
+                      : null,
+                    type: "usdStableCoin",
+                  }}
+                />
+              </div>
+            </div>
+            <div className="div-table-row">
+              <div className="div-table-col-fix-wide">
+                <Price
+                  contract={{
+                    address: addressConfig["UsdEurFXRateContract"]
+                      ? addressConfig["UsdEurFXRateContract"]
+                      : null,
+                    type: "fx",
+                  }}
+                  onAddInfo={handleLogChange}
+                />
+              </div>
+              <div className="div-table-col-fix-wide">
+                <StableToken
+                  contract={{
+                    address: addressConfig["eurStableCoin"]
+                      ? addressConfig["eurStableCoin"]
+                      : null,
+                    type: "eurStableCoin",
+                  }}
+                />
+              </div>
+            </div>
           </div>
         ) : null}
         {activeTab === "Escrow" ? (
@@ -73,47 +97,6 @@ export default function Home() {
           </div>
         ) : null}
         <OutputWindow outputContent={log} />
-=======
-        <Header />
-        <div className="div-table">
-          <div className="div-table-row">
-            <div className="div-table-col-fix-wide">
-              <Price contract={{
-                address: addressConfig["teslaEquityPriceContract"]
-                  ? addressConfig["teslaEquityPriceContract"]
-                  : null,
-                type: "equity"
-              }} />
-            </div>
-            <div className="div-table-col-fix-wide">
-              <StableToken contract={{
-                address: addressConfig["usdStableCoin"]
-                  ? addressConfig["usdStableCoin"]
-                  : null,
-                type: "usdStableCoin"
-              }} />
-            </div>
-          </div>
-          <div className="div-table-row">
-            <div className="div-table-col-fix-wide">
-              <Price contract={{
-                address: addressConfig["UsdEurFXRateContract"]
-                  ? addressConfig["UsdEurFXRateContract"]
-                  : null,
-                type: "fx"
-              }} />
-            </div>
-            <div className="div-table-col-fix-wide">
-              <StableToken contract={{
-                address: addressConfig["eurStableCoin"]
-                  ? addressConfig["eurStableCoin"]
-                  : null,
-                type: "eurStableCoin"
-              }} />
-            </div>
-          </div>
-        </div>
->>>>>>> main
       </div>
     </>
   );
