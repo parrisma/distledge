b4b234fcd4d544290b28c4c5a7b3d56821022382
parris3142 <parris3142@hotmail.com>
2023-04-30 17:29:04 +0800

    Added info message component, but not used by anything at this point

---
+++ frontend/components/Info.js
@@ -0,0 +1,11 @@
+const Contract = (props) => {
+
+    return (
+        <div className="info-container">
+            <div className="info">
+                <p>INFO:: {props.message}</p>
+            </div></div>
+    );
+};
+
+export default Contract;
