34dcc7e2952d0876e9f6a6b3f947e7989c5d2e12
parris3142 <parris3142@hotmail.com>
2023-05-20 06:56:24 +0800

    Created function to mint Option NFT & tests, now ready for integration into WebServer, so it can both mint and save the NFT

--- webserver/test/contractLoad.js
+++ webserver/test/contractLoad.js
@@ -5,8 +5,8 @@ require('module-alias/register'); // npm i --save module-alias
 const { getDictionaryOfDeployedContracts } = require("@lib/deployedContracts");
 const { addressConfig } = require("@webserver/constants");
 const { namedAccounts } = require("@scripts/lib/accounts");
-const { getSignedHashOfOptionTerms } = require("@scripts/lib/signedValue");
 const { guid } = require("@lib/guid");
+const { mintERC721OptionNFT } = require("@lib/contracts/Options/ERC721OptionContractTypeOne");
 
 async function main() {
 
@@ -23,22 +23,9 @@ async function main() {
     */
     try {
         let erc721OptionContractTypeOne_2 = contractDict[addressConfig.erc721OptionContractTypeOne];
-        for (let i = 0; i < 100; i++) {
-            sig = await getSignedHashOfOptionTerms(guid(), managerAccount);
-            const txResponse = await erc721OptionContractTypeOne_2.mintOption(sig);
-            const txReceipt = await txResponse.wait();
-            var response = undefined;
-            for (var j = 0; j < txReceipt.events.length; j++) {
-                const e = txReceipt.events[j];
-                console.log(`Event [${JSON.stringify(e["event"])}]`);
-                if (e["event"] === "OptionMinted") {
-                    response = e["args"];
-                    console.log(`MintOption Response [${response}]`);
-                }
-            }
-            if (null === response) {
-                throw new Error(`Missing expected response from mintOption`);
-            }
+        for (let i = 0; i < 50; i++) {
+            const [mintedOptionId, hashOfTerms, response] = await mintERC721OptionNFT(erc721OptionContractTypeOne_2, guid(), managerAccount);
+            console.log(`URI of newly minted Option Id [${mintedOptionId}] NFT [${response}]`);
         }
     } catch (err) {
         throw new Error(`Failed to test mint an Option Type One NFT - with error :[${err.message}]`);
