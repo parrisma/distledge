11aae943837c280f8e08346f1bf2128c0741d88e
EdwinLu <edwinlu@EdwinLudeMacBook-Air.local>
2023-06-05 20:24:26 +0800

    remove the alert when clicking exercise

--- frontend/pages/tabs/buyer.js
+++ frontend/pages/tabs/buyer.js
@@ -60,7 +60,6 @@ const Contract = (props) => {
      *       - This means assigning the option NFT back to the seller & burning it
      *       - move the option value (if > 0) from seller to buyer
      */
-    alert("this is optionId:" + optionId);
 
     sendExerciseRequest(optionId, buyerAccount)
       .then((res) => {
